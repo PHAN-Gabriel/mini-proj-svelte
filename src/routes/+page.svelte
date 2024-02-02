c24cc9b886080e02e7a327f041922d00cd6efcef
Phan Gabriel <o22104011@info12-20.iut45.univ-orleans.fr>
2024-02-02 14:57:46 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Button from "@smui/button";
+    let labels = ["Un", "Deux", "Trois"];
+    let index = 0;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button on:click={() => {index = (index + 1) % 3; }} variant="raised">{labels[index]}</Button>
