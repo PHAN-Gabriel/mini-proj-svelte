6889776c9ad81955efc7aa55962e909bf536ed0a
Phan Gabriel <o22104011@info12-20.iut45.univ-orleans.fr>
2024-02-07 11:00:31 +0100

    Debut du formulaire inscription

---
+++ src/routes/Champ.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let type = 'text';
+    export let label = undefined;
+    export let value = undefined;
+    export let errors = undefined;
+
+    if (type == "submit") {
+        value = label;
+    }
+
+    function typeInput(node) {
+        node.type = type;
+    }
+</script>
+
+<style>
+    div {
+        text-align: right;
+        margin-top: 0.5em;
+    }
+</style>
+
+<div class="space-y-1">
+    {#if type != "submit"}
+        <label for={label}>{label} :</label>
+    {/if}
+
+    <input id="input_{label}" name="{label}" use:typeInput bind:value class="rounded-md w-full" placeholder="{label}">
+
+    {#if errors}
+        <p id="errors_{label}" class="text-sm text-red-600">{errors}</p>
+    {/if}
+</div>
---
+++ src/routes/page_inscription.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Champ from './Champ.svelte';
+</script>
+
+<style>
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    div {
+        border: solid;
+        padding: 2em;
+    }
+</style>
+
+<form method="POST">
+    <div>
+        <Champ label="Nom" />
+        <Champ label="Prénom" />
+        <Champ label="Pseudonyme" />
+        <Champ label="Mot de passe" />
+        <Champ label="Confirmer le mot de passe" />
+        <Champ label="Est commerçant" type="checkbox" />
+        <Champ label="Confirmer" type="submit" />
+    </div>
+</form>
