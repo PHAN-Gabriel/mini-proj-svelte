62b3b7d141fa774ac2a1d528d9b17a133168f9a8
Gabriel Phan <gabph89@gmail.com>
2024-02-10 01:47:59 +0100

    Formulaire inscription terminé

--- src/routes/Champ.svelte
+++ src/routes/Champ.svelte
@@ -1,33 +1,44 @@
 <script>
+    import { sha256 } from 'js-sha256';
+
     export let type = 'text';
     export let label = undefined;
     export let value = undefined;
     export let errors = undefined;
 
+    let pos_div = "start";
     if (type == "submit") {
         value = label;
+        pos_div = "end";
     }
 
     function typeInput(node) {
         node.type = type;
+
+        switch (type) {
+            case "text":
+            case "password":
+                node.className += "form-control w-100";
+                break;
+            case "submit":
+                node.className += "btn btn-success";
+                break;
+        }
     }
 </script>
 
-<style>
-    div {
-        text-align: right;
-        margin-top: 0.5em;
-    }
-</style>
-
-<div class="space-y-1">
-    {#if type != "submit"}
+<div class="text-{pos_div} mt-1">
+    {#if type == "checkbox"}
         <label for={label}>{label} :</label>
+        <input id="input_{label}" name="{label}" type="checkbox" bind:checked={value}>
+    {:else if type == "password"}
+        <input id="input_{label}" name="{label}" use:typeInput on:change={() => value = sha256(value)} placeholder="{label}" required>
+    {:else}
+        <input id="input_{label}" name="{label}" use:typeInput bind:value placeholder="{label}" required>
     {/if}
 
-    <input id="input_{label}" name="{label}" use:typeInput bind:value class="rounded-md w-full" placeholder="{label}">
 
     {#if errors}
-        <p id="errors_{label}" class="text-sm text-red-600">{errors}</p>
+        <span id="errors_{label}" class="text-danger">{errors}</span>
     {/if}
 </div>
