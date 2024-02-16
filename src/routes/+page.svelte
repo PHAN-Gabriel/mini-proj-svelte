c388c4566adb7c554a1566301f57eaac13e6f613
jonathan.contois <jonathan.contois@etu.univ-orleans.fr>
2024-02-16 20:58:49 +0100

    creation page accueil + liaison

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
 <script>
     import Button from "@smui/button";
-    let labels = ["Un", "Deux", "Trois"];
-    let index = 0;
+    import AccueilAdmin from "./accueilAdmin.svelte";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Button on:click={() => {index = (index + 1) % 3; }} variant="raised">{labels[index]}</Button>
+<header class="header">
+    %sveltekit.head%
+</header>
+
+<AccueilAdmin/>
+
+<style>
+    .header {
+        background-color: black;
+    }
+</style>
